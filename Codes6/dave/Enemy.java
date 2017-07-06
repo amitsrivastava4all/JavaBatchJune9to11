c3877e2acd1a1fc7f64b524919ed00795c4eec7b
amit4alljava <amit4alljava@gmail.com>
2017-07-06 11:24:43 +0530

    Codes

---
+++ Codes6/dave/Enemy.java
@@ -0,0 +1,33 @@
+package com.bmpl.dave;
+
+import java.awt.Graphics;
+import java.awt.Image;
+
+public class Enemy extends Sprite implements GameConstants {
+	
+	public Enemy(int x , Image image, int speed){
+		this.x = x;
+		this.img = image;
+		this.y = 30;
+		this.speed = speed;
+		h = w = 100;
+	}
+	public void drawEnemy(Graphics g){
+		g.drawImage(img, x, y, w, h, null);
+		move();
+		changeDirection();
+	}
+	public void move(){
+		y +=speed;
+	}
+	public void changeDirection(){
+		if(y>=(FLOOR-h)){
+			speed = speed * -1;
+		}
+		else
+		if(y<=0)
+		{
+			speed = speed * -1;
+		}
+	}
+}
